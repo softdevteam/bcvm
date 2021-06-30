d387047a6dee513bf29e1ceb3a2d0c585d4e4fc8
Michael Trent <mtrent133@gmail.com>
2021-06-30 18:07:45 +0100

    Implement some load instructions and some calls to 'libc::printf'.
    Interpreting external function calls properly is quite complex, so for now we hardcode interpreting some basic 'printf' calls in order to run 'lang_tester' tests.

--- lang_tests/simple.c
+++ lang_tests/simple.c
@@ -1,6 +1,16 @@
 // Compiler: 
 // Runtime:
+//    stdout: 1
+//            noargs
+//            1 arg: 1
+//            2 args: 1 2
+//            string arg: string
 int main() {
    int a = 1;
+   printf("%i", a);
+   printf("noargs");
+   printf("1 arg: %i", 1);
+   printf("2 args: %i %i", 1, 2);
+   printf("string arg: %s", "string");
    return 0;
 }
